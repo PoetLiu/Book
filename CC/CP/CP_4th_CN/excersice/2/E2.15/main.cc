b2a430a32d95f44873eb4960a802db1916f9cc38
LP <457566268@qq.com>
2017-08-16 17:51:41 +0800

    Optimize.

--- CC/CP/CP_4th_CN/excersice/2/E2.15/main.cc
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main(void)
-{
-	int month = 09, day = 07;
-	return 0;
-}
--- CC/CP/CP_5th_EN/dir.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for ((i = 1; i < 18; i++))
-do	
-	if [ $i < 10 ]; then
-		mkdir "ch0$i";
-	else
-		mkdir "ch$i";
-	fi
-done
